1b276c1909903457c8af3f144c20ad4b43a01d8c
Qentin28 <miliankolp@wp.pl>
2017-01-12 11:12:26 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# html
+# Amazing-Program
+Best program EU
---
+++ amazingprogram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace amazingprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, c;
+            Console.WriteLine("Wprowasdfgsdgdz pierwsza liczbe");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wprowadsfgsdz druga liczbe");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wynsdfggiksxxx = " + (a+c));
+            Console.ReadKey();
+        }
+    }
+}
